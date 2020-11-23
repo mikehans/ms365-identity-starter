39f1f819cffc1e64669ab088a0bbf71018aafc35
Mike Hansford <michael.hansford@team.telstra.com>
2020-11-23 11:17:31 +1100

    Emergency failing HDD commit

---
+++ build/startMessage.js
@@ -0,0 +1,6 @@
+// import chalk from 'chalk';
+const chalk = require('chalk');
+
+/* eslint-disable no-console */
+
+console.log(chalk.green('Starting dev server'));
---
+++ webpack.config.dev.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: { cacheDirectory: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html', filename: './index.html' })
+  ]
+};
